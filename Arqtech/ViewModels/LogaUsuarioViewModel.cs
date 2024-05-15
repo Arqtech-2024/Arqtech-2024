d5c55281602e2972f9323d22b899423505640cc5
Caio Maestrello <projetoarqtech@outlook.com>
2024-05-15 02:11:10 -0300

    inclusão do template.

--- Arqtech/ViewModels/LogaUsuarioViewModel.cs
+++ Arqtech/ViewModels/LogaUsuarioViewModel.cs
@@ -1,8 +1,13 @@
-﻿namespace Arqtech.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Arqtech.ViewModels
 {
     public class LogaUsuarioViewModel
     {
+        [Required(ErrorMessage = "O campo de E-mail é obrigatório")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo de senha é obrigatório")]
         public string Password { get; set; }
     }
 }
