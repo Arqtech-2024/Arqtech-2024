e31da38b19d0a7cc5385cd728925be3cd267b7ce
Dhiego Arsuffi <arsuffi_dhiego@outlook.com>
2024-05-28 20:42:53 -0300

    alterações das views e add atalho para "voltar a página"

--- Arqtech/Controllers/UsuarioController.cs
+++ Arqtech/Controllers/UsuarioController.cs
@@ -112,7 +112,7 @@ namespace Arqtech.Controllers
         public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login");
         }
 
         [AllowAnonymous]
